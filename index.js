09c22daf0c8a65c01885986e1a077a6ccd3a86e6
edniaj <jiandegoh@gmail.com>
2022-04-26 21:46:16 +0800

    Commented out some code so that app will run

--- index.js
+++ index.js
@@ -60,70 +60,65 @@ app.use(session({
 app.use(flash());
 
 // display in the hbs file
-app.use(function(req,res,next){
-    // transfer any success messages stored in the session
-    // to the variables in hbs files
-    res.locals.success_messages = req.flash("success_messages");
-    res.locals.error_messages = req.flash('error_messages');
-    next();    
-})
+// app.use(function(req,res,next){
+//     // transfer any success messages stored in the session
+//     // to the variables in hbs files
+//     res.locals.success_messages = req.flash("success_messages");
+//     res.locals.error_messages = req.flash('error_messages');
+//     next();    
+// })
+
 
 // app.use(csrf());
-const csurfInstance = csrf();  // creating a prox of the middleware
-app.use(function(req,res,next){
-    // if it is webhook url, then call next() immediately
-    // or if the url is for the api, then also exclude from csrf
-    if (req.url === '/checkout/process_payment' || 
-        req.url.slice(0,5)=='/api/') {
-        next();
-    } else {
-        csurfInstance(req,res,next);
-    }
+// const csurfInstance = csrf();  // creating a prox of the middleware
+// app.use(function(req,res,next){
+//     // if it is webhook url, then call next() immediately
+//     // or if the url is for the api, then also exclude from csrf
+//     if (req.url === '/checkout/process_payment' || 
+//         req.url.slice(0,5)=='/api/') {
+//         next();
+//     } else {
+//         csurfInstance(req,res,next);
+//     }
 
 
-})
+// })
 
-// middleware to share the csrf token with all hbs files
-app.use(function(req,res,next){
-    // the req.csrfToken() generates a new token
-    // and save its to the current session
-    if (req.csrfToken) {
-        res.locals.csrfToken = req.csrfToken();
-    }
-    next();
-})
+// // middleware to share the csrf token with all hbs files
+// app.use(function(req,res,next){
+//     // the req.csrfToken() generates a new token
+//     // and save its to the current session
+//     if (req.csrfToken) {
+//         res.locals.csrfToken = req.csrfToken();
+//     }
+//     next();
+// })
 
-// middleware to handle csrf errors
-// if a middleware function takes 4 arguments
-// the first argument is error
-app.use(function(err, req,res,next){
-    if (err && err.code == "EBADCSRFTOKEN") {
-        req.flash('error_messages', "The form has expired. Please try again");
-        res.redirect('back'); // go back one page
-    } else {
-        next();
-    }
-})
+// // middleware to handle csrf errors
+// // if a middleware function takes 4 arguments
+// // the first argument is error
+// app.use(function(err, req,res,next){
+//     if (err && err.code == "EBADCSRFTOKEN") {
+//         req.flash('error_messages', "The form has expired. Please try again");
+//         res.redirect('back'); // go back one page
+//     } else {
+//         next();
+//     }
+// })
 
 // share the details of the logged in user with all routes
-app.use(function(req,res,next){
-    res.locals.user = req.session.user;
-    next();
-})
+// app.use(function(req,res,next){
+//     res.locals.user = req.session.user;
+//     next();
+// })
 
 
-// IMPORT IN THE ROUTES
-const landingRoutes = require('./routes/landing');
-const productRoutes = require('./routes/products');
-const userRoutes = require('./routes/users');
-const cloudinaryRoutes = require('./routes/cloudinary')
-const shoppingCartRoutes = require('./routes/shoppingCart');
-const checkoutRoutes = require('./routes/checkout')
 
-// CREATE API ROUTES
+
+// CREATE PUBLIC API ROUTES
 const api = {
-    products: require('./routes/api/products'),
-    users: require('./routes/api/users')
+    // products: require('./routes/api/products'),
+    // users: require('./routes/api/users')
 }
 
 const { checkIfAuthenticated } = require('./middlewares');
@@ -132,17 +127,13 @@ const { checkIfAuthenticated } = require('./middlewares');
 
 async function main(){
     app.get('/', function(req,res){
-                res.redirect('/landing');
+                res.send("Welcome")
             })
 
-    app.use('/landing', landingRoutes);
-    app.use('/products', productRoutes);
-    app.use('/users', userRoutes);
-    app.use('/cloudinary', cloudinaryRoutes);
-    app.use('/cart', checkIfAuthenticated ,  shoppingCartRoutes);
-    app.use('/checkout', checkoutRoutes);
-    app.use('/api/products', express.json(), api.products);
-    app.use('/api/users', express.json(), api.users);
+    // app.use('/cart', checkIfAuthenticated ,  shoppingCartRoutes);
+    // app.use('/checkout', checkoutRoutes);
+    // app.use('/api/products', express.json(), api.products); // api means front facing
+    // app.use('/api/users', express.json(), api.users);
 
 }
 main();
@@ -156,6 +147,6 @@ main();
 //     })
 // })();
 
-app.listen(3000,function(req,res){
+app.listen(process.env.PORT,function(req,res){
     console.log("Server started");
 })
